5acfc922288e7ef924291f6cc8f3f9a2f0d35f2a
Tim Batty <tbatty7@gmail.com>
2018-09-15 21:41:45 -0400

    Initial Commit

---
+++ src/test/java/com/simple/springboot/app/AppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.simple.springboot.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+
+}
